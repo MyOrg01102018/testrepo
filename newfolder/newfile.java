42342b7088e416c0669cfef1328f4204a1d27ec4
deeptiharinkhede <33171973+deeptiharinkhede@users.noreply.github.com>
2018-01-10 10:53:14 +0000

    Create newfile.java

---
+++ newfolder/newfile.java
@@ -0,0 +1,11 @@
+public class LogicalComponentCreationImpl extends AbstractService implements IOperationParticipant {
+
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void run(AdvisableOperation operation,
+			IProcessConfigurationElement participantConfig,
+			IParticipantInfoCollector collector, IProgressMonitor monitor)
+			throws TeamRepositoryException {
+      }
+      }
